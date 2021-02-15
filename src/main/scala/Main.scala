b6c2b815cdfc54c2eebc8c8f49f527f0b9f2cfcb
MrMoonshine <mrmoonshine.goo@gmail.com>
2021-02-15 11:00:16 +0100

    Imported ScalaFx

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,11 @@
 
 object Main{
   def main(args: Array[String]): Unit = {
-    val filegz:String = "assets/MorseserverThreaded_0.tar.gz"
-    val fileoida:String = "assets/targarbage.tar.gz"
-    val filexz:String = "assets/waifus.tar.xz"
+    //val filegz:String = "assets/MorseserverThreaded_0.tar.gz"
+    //val fileoida:String = "assets/targarbage.tar.gz"
+    //val filexz:String = "assets/waifus.tar.xz"
 
-    new UI
+    //new UI
+
   }
 }
